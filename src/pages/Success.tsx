52b8726c5ed192d9b2bf5c2ac266588ee6f8a4f9
Sudipta Das <shiningsudipto@gmail.com>
2024-07-13 01:34:07 +0600

    payment successful page added

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import psImg from "../assets/images/ps1.png";
 const Success = () => {
   return (
     <div>
-      <p>Hello, Success!</p>
+      <div className="flex flex-col items-center justify-center space-y-4 my-[60px]">
+        <h3 className="text-4xl font-bold text-primaryColor">
+          Payment Successful
+        </h3>
+        <Link
+          to={"/products"}
+          className="font-semibold text-xl text-secondaryColor"
+        >
+          Brows more products!
+        </Link>
+        <Link to={"/products"} className="">
+          <img src={psImg} alt="" className="h-[200px]" />
+        </Link>
+      </div>
     </div>
   );
 };
